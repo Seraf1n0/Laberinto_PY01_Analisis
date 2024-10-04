60c668a8511aa7fd1fb544b8a7193974b005aa99
SERAFINO <ymontenegro@estudiantec.cr>
2024-10-04 05:09:19 -0600

    Propuesta de resolución utilizando el diccionario

--- app.py
+++ app.py
@@ -8,8 +8,6 @@ def index():
     lab = Laberinto(10, 10)  # Laberinto 10x10
     lab.generarMatriz()  # Genera el laberinto
     # Prueba para normalización
-    lab.normalizarLaberinto()
-    lab.imprimirNormalizado()
     matriz = lab.obtenerMatriz()  # Obtiene la matriz del laberinto
     return render_template('index.html', matriz=matriz)  # Pasa la matriz al template
 
