7e0f4acb975dad7177bd070eed7b73b83ef37452
Dylan Rodríguez <dylrodriguez@estudiantec.cr>
2024-10-03 22:54:49 -0600

    Vuelvo a agregar el cargar laberinto de archivo

--- laberinto/laberinto.py
+++ laberinto/laberinto.py
@@ -17,20 +17,37 @@ class ElementoLaberinto:
         }
 
 class Laberinto:
-    def __init__(self, altura, ancho) :
+    def __init__(self, altura, ancho, rutaArchivo=None) :
         #Poniendo atributos de instancia
-        self.ancho = ancho
-        self.altura = altura
-        self.matriz = []
-        self.direcciones = ['n', 's', 'e', 'o'] #norte, sur, este, oeste
-        self.modificarDireccion = {
-            'n': {'fila': -1, 'columna': 0,'opuesto':'s'},
-            's': {'fila': 1, 'columna': 0,'opuesto':'n'},
-            'e': {'fila': 0, 'columna': 1,'opuesto':'o'},
-            'o': {'fila': 0, 'columna': -1,'opuesto':'e'}
-        }
-        self.cantidadCeldas = ancho * altura
-        self.celdasVisitadas = 1
+        if(rutaArchivo is None): #No se proporciona la ruta del archivo, entonces hay que crear la matriz de 0
+            self.ancho = ancho
+            self.altura = altura
+            self.matriz = []
+            self.direcciones = ['n', 's', 'e', 'o'] #norte, sur, este, oeste
+            self.modificarDireccion = {
+                'n': {'fila': -1, 'columna': 0,'opuesto':'s'},
+                's': {'fila': 1, 'columna': 0,'opuesto':'n'},
+                'e': {'fila': 0, 'columna': 1,'opuesto':'o'},
+                'o': {'fila': 0, 'columna': -1,'opuesto':'e'}
+            }
+            self.cantidadCeldas = ancho * altura
+            self.celdasVisitadas = 1
+            self.generarMatriz() #Para generar la matriz aleatoria
+        else:
+            self.ancho = 0
+            self.altura = 0
+            self.matriz = []
+            self.direcciones = ['n', 's', 'e', 'o'] #norte, sur, este, oeste
+            self.modificarDireccion = {
+                'n': {'fila': -1, 'columna': 0,'opuesto':'s'},
+                's': {'fila': 1, 'columna': 0,'opuesto':'n'},
+                'e': {'fila': 0, 'columna': 1,'opuesto':'o'},
+                'o': {'fila': 0, 'columna': -1,'opuesto':'e'}
+            }
+
+            #Para cargar la matriz
+            self.leerLaberintoDeArchivo(rutaArchivo)
+            self.cantidadCeldas = self.ancho * self.altura
 
 
     def generarMatriz(self):
@@ -69,6 +86,7 @@ class Laberinto:
             if(ciclos > maximoCiclos):
                 random.shuffle(self.direcciones)
                 maximoCiclos = round(random.random() * (self.altura / 8))
+                ciclos = 0
             ciclos +=1
             #Ciclo para decidir el movimiento
             for direccion in self.direcciones:
@@ -204,6 +222,77 @@ class Laberinto:
                     archivo.write(lineaEscribir)
 
         archivo.close()
-         
+
+    def leerLaberintoDeArchivo(self, rutaArchivo):
+        if(self.totalLineasArchivo(rutaArchivo) > 0):
+            fila = 0
+            columna = 0
+            with open(rutaArchivo, 'r') as archivo:
+                primeraLinea = True
+                lineaDimensiones = True
+                for linea in archivo:
+                    if(primeraLinea):
+                        #Estoy en la primera línea, verifico que el texto sea el correcto
+                        if(linea.rstrip('\n') == "" or linea.rstrip('\n')  != "Laberinto"):
+                            return False #No es un archivo apto para leersh
+                        primeraLinea = False
+                    else:
+                        #Después de la primera línea tengo que leer la cantidad de filas y columnas
+                        if(lineaDimensiones):
+                            listaDimensiones = linea.rstrip('\n').split(",")
+                            self.altura, self.ancho = int(listaDimensiones[0]), int(listaDimensiones[1]) #Filas, columnas
+                            self.matriz = [[ElementoLaberinto() for _ in range(self.ancho)] for _ in range(self.altura)]
+                            lineaDimensiones = False
+                        else:
+                            #Ya tocaría leersh todas las demás líneas
+                        
+                            listaParedes = linea.rstrip('\n').split(",")
+                            #Tengo que poner los boleanos indicando las paredes
+                            if(listaParedes[1] == "True"):
+                                paredN = True
+                            else:
+                                paredN = False
+                                
+                            if(listaParedes[3] == "True"):
+                                paredS = True
+                            else:
+                                paredS = False
+
+                            if(listaParedes[5] == "True"):
+                                paredE = True
+                            else:
+                                paredE = False
+                                    
+                            if(listaParedes[5] == "True"):
+                                paredO = True
+                            else:
+                                paredO = False
+
+                            #Ahora añado todos los valores al diccionario
+                            self.matriz[fila][columna].caminos[listaParedes[0]]['camino'] = paredN
+                            self.matriz[fila][columna].caminos[listaParedes[2]]['camino'] = paredS
+                            self.matriz[fila][columna].caminos[listaParedes[4]]['camino'] = paredE
+                            self.matriz[fila][columna].caminos[listaParedes[6]]['camino'] = paredO 
+                            
+                            columna += 1
+                            if(columna == self.ancho):
+                                columna = 0
+                                fila +=1
+
+
+        else:
+            return False #El archivo no tenía líneas para leersh
+
+    def totalLineasArchivo(self, rutaArchivo):
+        contador = 0
+        archivo = open(rutaArchivo, mode="r")
+        #contenido = archivo.read()
+        listaLineas = archivo.readlines()
+        for linea in listaLineas:
+            contador += 1
+        return contador
+
     def obtenerMatriz(self):
         return self.matriz
+    
+a = Laberinto(5,5)
