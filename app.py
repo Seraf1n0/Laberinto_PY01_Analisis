578761645fe963fe47533186aa4e47f08e9af331
Dylan Rodríguez <dylrodriguez@estudiantec.cr>
2024-10-04 21:33:26 -0600

    Se agregan los comentarios restantes de la app

--- app.py
+++ app.py
@@ -33,10 +33,9 @@ def index(laberinto = None, mensajeDeError = None):
         return render_template('index.html', matriz=matriz, tamanio=lab.altura, mensajeError = False)  # Pasa la matriz al template
         
     else:
-        #Tendría que mandarle el laberinto cargado
         if(mensajeDeError is not None):
             #Tengo que mostrar el mensaje de error
-            lab = Laberinto(10, 10)  # Laberinto 10x10
+            lab = Laberinto(10, 10)  # Laberinto 10x10 predeterminado
             matrizG = lab
             matriz = lab.obtenerMatriz()  
             return render_template('index.html', matriz=matriz, tamanio=lab.altura, mensajeError = True)  # Pasa la matriz al template
@@ -48,7 +47,12 @@ def index(laberinto = None, mensajeDeError = None):
         
     
     
-
+"""
+Esta función se ejecuta cuando se presiona el botón Guadar Laberinto en la interfaz.
+Toma la matriz y ejecuta el método que permite guardar el laberinto.
+El archivo lo pone en la carpeta preparando que se encuentra en el directorio del proyecto. Antes de guardarlo borra todos los anteriores para que solo quede el últio generado.
+Luego el archivo se descarga en la interfaz.
+"""
 @app.route('/guardarLaberinto', methods=['POST'])
 def guardarLaberinto():
     
@@ -65,19 +69,22 @@ def guardarLaberinto():
 
     matrizG.guardarEnArchivo(file_path)
     return send_file(file_path, as_attachment=True)
-    
+
+
+"""
+Esta función se ejecuta cuando se presiona el botón Subir. Previamente debe haberse seleccionado el archivo a cargar.
+Se eliminan todos los archivos que se encuentren en la carpeta uploads y luego se agrega el nuevo archivo generado por el método en la clase Laberinto
+"""
 @app.route('/upload', methods=['POST'])
 def upload_file():
  
     if 'file' not in request.files:
-        return "No file part in the request"
+        return "El archivo no se encuentra entre los solicitados"
     
     file = request.files['file']
-    
-    
 
     if file.filename == '':
-        return index(1, True)
+        return index(1, True) #Por si no ha seleccionado aún
     
     # Eliminar todos los archivos existentes en la carpeta
     for filename in os.listdir(app.config['UPLOAD_FOLDER']):
@@ -87,7 +94,7 @@ def upload_file():
             if os.path.isfile(file_path):
                 os.remove(file_path)
         except Exception as e:
-            return f"Error deleting file {filename}: {e}"
+            return f"Error borrando archivo {filename}: {e}"
 
     
     # Guardar el nuevo archivo
@@ -107,6 +114,12 @@ def upload_file():
             #Hago el return pero con el parámetro para que me muestre el mensaje de error
             return index(matrizLocal, True)
 
+
+"""
+Esta función se ejecuta al momento de presionar el botón de Generar Laberinto en la interfaz.
+Se toma el valor que se encuentra en el spinbox con el tamaño del laberinto y se crea este.
+Luego se envía a la interfaz para que se cargue
+"""
 @app.route('/generarLaberinto', methods=['POST'])
 def generarLaberinto():
     tamano = request.form.get('tamanoLaberinto', type=int) #Con esto obtengo la selección del spinbox
